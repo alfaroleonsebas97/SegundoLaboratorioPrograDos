782134a08d2aaa7b9c1eb2edd0bcaf439d8d5bdc
alfaroleonsebas97 <37912510+alfaroleonsebas97@users.noreply.github.com>
2018-05-31 20:18:33 -0600

    Update Laberinto.cpp

--- Laberinto.cpp
+++ Laberinto.cpp
@@ -150,7 +150,7 @@ void Laberinto::asgIdVrtFinal(int idVrtFinalN) {
 }
 
 void Laberinto::asgDatoAdy(int idVrtO, int idVrtD, const Adyacencia& ady) {
-    if(xstVrt(idVrtO) && xstVrt(idVrtD) ){
+    if( (xstVrt(idVrtO)) && (xstVrt(idVrtD)) ){
         datosAdys.insert( map<int,Adyacencia>::value_type( obtIndiceAdy(idVrtO,idVrtD), ady) );
     }
 }
