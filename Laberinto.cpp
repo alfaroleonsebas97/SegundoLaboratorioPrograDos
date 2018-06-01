5824eba93387e863d4c2ade1b8d4f96bc7b54b14
JCRP444 <39810042+JCRP444@users.noreply.github.com>
2018-05-31 18:28:20 -0600

    Update Laberinto.cpp

--- Laberinto.cpp
+++ Laberinto.cpp
@@ -91,7 +91,7 @@ bool Laberinto::xstAdy(int idVrtO, int idVrtD) const {
     if ((xstVrt(idVrtO)) && (xstVrt(idVrtD))) {                             //si ambos vértices existen,
         //adyacencia = arregloVrts[idVrtO].lstAdy.buscar(idVrtD);             //busca si hay adyacencia entre el vértice origen y el destino.
     for(int i = 0; i < vertices[idVrtO].lstAdy.size();i++){
-        if(idVrtD = vertices[idVrtO].lstAdy[i]) {
+        if(idVrtD == vertices[idVrtO].lstAdy[i]) {
             adyacencia = true;
         }    
     }
